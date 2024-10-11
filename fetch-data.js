8771d2776a7f7029e107d166239fb14054341680
Peggy Annan <pegannan.22@gmail.com>
2024-10-11 14:15:44 +0000

    fetch-data files

---
+++ fetch-data.js
@@ -0,0 +1,23 @@
+async function fetchUserData() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+   const dataContainer = document.getElementById("api-data")
+
+   try{
+    const response = await fetchUserData((apiUrl) => {
+        const users = await.response.JSON})
+        array.forEach(element => {
+            <li>user's name</li>
+        });;
+
+        dataContainer.innerHTML = ''
+
+        const userList = <ul><li></li></ul>
+
+    catch{
+        dataContainer.innerHTML = 'Failed to load user data.'
+    }
+   };
+}
+
+document.addeventlistener("DOMContentLoaded", (event) => callback(fetchUserData))
